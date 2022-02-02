175c2eb301c3633ab7d3e3a8cb1dd7fe39b35a3a
Ulan Chokushov <ulan.chokushov@gmail.com>
2022-02-02 15:32:56 +0600

    added some changes

--- movies/views.py
+++ movies/views.py
@@ -154,6 +154,6 @@ class MoviesView(View):
 
 
 class MovieDetailView(View):
-    def get(self, request, pk):
-        movie = Movie.objects.get(id=pk)
+    def get(self, request, slug):
+        movie = Movie.objects.get(url=slug)
         return render(request, 'movies/moviesingle.html', {'movie': movie})
