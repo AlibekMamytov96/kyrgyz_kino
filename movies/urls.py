369700a63430872a569f764de3b96d33e2564ccb
Ulan Chokushov <ulan.chokushov@gmail.com>
2022-02-02 15:27:46 +0600

    changed some html files

--- movies/urls.py
+++ movies/urls.py
@@ -5,7 +5,7 @@ from .import views
 
 urlpatterns = [
     path('', views.MoviesView.as_view()),
-    path('<int: pk>/', views.MovieDetailView.as_view()),
+    path('<int:pk>/', views.MovieDetailView.as_view()),
     # path("filter/", views.FilterMoviesView.as_view(), name='filter'),
     # path("search/", views.Search.as_view(), name='search'),
     # path("add-rating/", views.AddStarRating.as_view(), name='add_rating'),
