66af72287f6a756eed6856f6b736e13bf8fccab5
Ulan Chokushov <ulan.chokushov@gmail.com>
2022-02-04 15:19:24 +0600

    added favorites

--- favorites/urls.py
+++ favorites/urls.py
@@ -5,9 +5,9 @@ from favorites.views import favorites_list, add_to_favorites, remove_from_favori
 app_name = 'favorites'
 
 urlpatterns = [
-    path('favorites/', include([
+        path('favorites/', include([
         path('', favorites_list, name='list'),
-        path('<id>/remove/', remove_from_favorites, name='add'),
+        path('<id>/add/', add_to_favorites(), name='add'),
         path('<id>/remove/', remove_from_favorites, name='remove'),
         path('delete/', delete_favorites, name='delete'),
     ]))
